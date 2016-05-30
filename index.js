9f07b2921bd0427549e9f7b3b67d2fc54060dae6
Aaron Sikes <aaron@sikes.io>
2016-05-30 11:40:40 -0400

    feat: Run a hoodie server in development

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-/* jshint node: true */
+/* jshint node:true */
 'use strict';
+
+var getHoodieServer = require('hoodie');
+var url = require('url');
+var proxy = require('http-proxy-middleware');
+
+var hoodie_options = {
+  port: 4201
+}
 
 module.exports = {
   name: 'ember-hoodie',
@@ -8,5 +16,22 @@ module.exports = {
     this._super(...arguments);
 
     app.import('vendor/hoodie.js');
+  },
+
+  serverMiddleware(config) {
+    config.app.use('/hoodie', proxy({target: 'http://localhost:' + hoodie_options.port}));
+
+    getHoodieServer(hoodie_options, function (error, server, config) {
+      if (error) {
+        var stack = new Error().stack.split('\n').slice(2).join('\n')
+        return console.log('app', 'Failed to initialise:\n' + stack, error)
+      }
+
+      console.log('app', 'Starting')
+
+      server.start(function () {
+        console.log('Your Hoodie server has started on ' + url.format(config.connection))
+      })
+    })
   }
 };
