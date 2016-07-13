796d7213010b8937b0b6d234fae9d64e42442451
Gregor <gregor@martynus.net>
2016-07-13 17:42:58 -0400

    style: jshint

--- index.js
+++ index.js
@@ -11,23 +11,23 @@ var Funnel = require('broccoli-funnel');
 
 var hoodie_options = {
   port: 4201
-}
+};
 
 function hoodieMiddleware(config) {
   config.app.use('/hoodie', proxy({target: 'http://localhost:' + hoodie_options.port}));
 
   getHoodieServer(hoodie_options, function (error, server, config) {
     if (error) {
-      var stack = new Error().stack.split('\n').slice(2).join('\n')
-      return console.log('app', 'Failed to initialise:\n' + stack, error)
+      var stack = new Error().stack.split('\n').slice(2).join('\n');
+      return console.log('app', 'Failed to initialise:\n' + stack, error);
     }
 
-    console.log('app', 'Starting')
+    console.log('app', 'Starting');
 
     server.start(function () {
-      console.log('Your Hoodie server has started on ' + url.format(config.connection))
-    })
-  })
+      console.log('Your Hoodie server has started on ' + url.format(config.connection));
+    });
+  });
 }
 
 module.exports = {
@@ -50,7 +50,7 @@ module.exports = {
   },
 
   included(app) {
-    this._super.apply(this, arguments)
+    this._super.apply(this, arguments); 
 
     app.import('vendor/hoodie.js');
   },
