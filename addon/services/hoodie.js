9a3b4f4733833d18be05a1a0c3b7cd9b10a0524e
Gregor <gregor@martynus.net>
2016-08-27 13:08:45 -0700

    fix: make ENV.hoodie.client config optional

--- addon/services/hoodie.js
+++ addon/services/hoodie.js
@@ -9,15 +9,16 @@ const {
 export default Service.extend({
   init() {
     this._super(...arguments);
-    let config = Ember.getOwner(this).application.resolveRegistration('config:environment').hoodie.client
-    let hoodie = new Hoodie(config);
+    const appConfig = Ember.getOwner(this).application.resolveRegistration('config:environment')
+    const hoodieConfig = appConfig.hoodie ? appConfig.hoodie.client : {}
+    const hoodie = new Hoodie(hoodieConfig);
     set(this, 'hoodie', hoodie);
     // for debug only
     window.hoodie = hoodie;
   },
 
   unknownProperty(keyName) {
-    let hoodie = get(this, 'hoodie');
+    const hoodie = get(this, 'hoodie');
     if (keyName in hoodie) {
       return hoodie[keyName];
     } else {
